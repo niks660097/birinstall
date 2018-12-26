691978c94c8cc3a774e481aae025a78a33f26296
nirmal singh <nirmal@bitfia.io>
2018-12-26 17:50:35 +0530

    dosetup removed

--- bitcoin_green.sh
+++ bitcoin_green.sh
@@ -14,53 +14,53 @@ echo "!                                                 !"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo && echo && echo
 
-echo "Do you want to install all needed dependencies (no if you did it before)? [y/n]"
-read DOSETUP
+#echo "Do you want to install all needed dependencies (no if you did it before)? [y/n]"
+#read DOSETUP
 
-if [[ $DOSETUP =~ "y" ]] ; then
-  sudo apt-get update
-  sudo apt-get -y upgrade
-  sudo apt-get -y dist-upgrade
-  sudo apt-get install -y nano htop git
-  sudo apt-get install -y software-properties-common
-  sudo apt-get install -y build-essential libtool autotools-dev pkg-config libssl-dev
-  sudo apt-get install -y libboost-all-dev
-  sudo apt-get install -y libevent-dev
-  sudo apt-get install -y libminiupnpc-dev
-  sudo apt-get install -y autoconf
-  sudo apt-get install -y automake unzip
-  sudo add-apt-repository  -y  ppa:bitcoin/bitcoin
-  sudo apt-get update
-  sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
+#if [[ $DOSETUP =~ "y" ]] ; then
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get install -y nano htop git
+sudo apt-get install -y software-properties-common
+sudo apt-get install -y build-essential libtool autotools-dev pkg-config libssl-dev
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y libevent-dev
+sudo apt-get install -y libminiupnpc-dev
+sudo apt-get install -y autoconf
+sudo apt-get install -y automake unzip
+sudo add-apt-repository  -y  ppa:bitcoin/bitcoin
+sudo apt-get update
+sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
 
-  cd /var
-  sudo touch swap.img
-  sudo chmod 600 swap.img
-  sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
-  sudo mkswap /var/swap.img
-  sudo swapon /var/swap.img
-  sudo free
-  sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
-  cd
+cd /var
+sudo touch swap.img
+sudo chmod 600 swap.img
+sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+sudo mkswap /var/swap.img
+sudo swapon /var/swap.img
+sudo free
+sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+cd
 
-  wget https://github.com/bitcoingreen/bitcoingreen/releases/download/v1.3.0/bitcoingreen-1.3.0-x86_64-linux-gnu.tar.gz
-  tar -xzf bitcoingreen-1.3.0-x86_64-linux-gnu.tar.gz
-  chmod 755 bitcoingreen-1.3.0/bin/*
-  rm /usr/bin/bitcoingreen*
-  mv bitcoingreen-1.3.0/bin/bitcoingreen* /usr/bin
-  rm -r bitcoingreen-1.3.0
+wget https://github.com/bitcoingreen/bitcoingreen/releases/download/v1.3.0/bitcoingreen-1.3.0-x86_64-linux-gnu.tar.gz
+tar -xzf bitcoingreen-1.3.0-x86_64-linux-gnu.tar.gz
+chmod 755 bitcoingreen-1.3.0/bin/*
+rm /usr/bin/bitcoingreen*
+mv bitcoingreen-1.3.0/bin/bitcoingreen* /usr/bin
+rm -r bitcoingreen-1.3.0
 
-  sudo apt-get install -y ufw
-  sudo ufw allow ssh/tcp
-  sudo ufw limit ssh/tcp
-  sudo ufw logging on
-  echo "y" | sudo ufw enable
-  sudo ufw status
+sudo apt-get install -y ufw
+sudo ufw allow ssh/tcp
+sudo ufw limit ssh/tcp
+sudo ufw logging on
+echo "y" | sudo ufw enable
+sudo ufw status
 
-  mkdir -p ~/bin
-  echo 'export PATH=~/bin:$PATH' > ~/.bash_aliases
-  source ~/.bashrc
-fi
+mkdir -p ~/bin
+echo 'export PATH=~/bin:$PATH' > ~/.bash_aliases
+source ~/.bashrc
+#fi
 
 ## Setup conf
 mkdir -p ~/bin
