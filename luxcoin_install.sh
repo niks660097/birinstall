b37f39430b55913db68859585f003b49a1b364a0
Nirmal Singh <niks660097@gmail.com>
2019-01-08 15:49:31 +0530

    Update luxcoin_install.sh

--- luxcoin_install.sh
+++ luxcoin_install.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+
 wget https://github.com/LUX-Core/lux/archive/v5.2.3.tar.gz
 mkdir -p lux && tar xf tarfilename -C ./lux --strip-components=1
 sudo find lux/ -type f -iname "*.sh" -exec chmod +x {} \;
 cd lux/
 sudo sh ./depends/install-dependencies.sh
 sudo ./autogen.sh && sudo ./configure --disable-tests --without-gui && sudo make clean && sudo make -j$(nproc)
+cd $HOME
 mkdir ~/.lux
+CONFIGFOLDER=/root/.lux
+CONFIG_FILE=lux.conf
 NODEIP=$(curl -s4 icanhazip.com)
 PRIVATE_KEY="$1"
 
