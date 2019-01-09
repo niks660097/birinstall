4a0d040526f63d62192f8db00fd6c51934c459fd
Nirmal Singh <niks660097@gmail.com>
2019-01-09 19:06:45 +0530

    Create exclusive_wallet_install.sh

---
+++ exclusive_wallet_install.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/exclusivecoin/Exclusive.git
+cd ExclusiveCoin/src
+sudo apt-get install libssl-dev
+sudo apt-get install libboost-dev
+sudo apt-get install libdb++-dev
+sudo make -f makefile.unix
