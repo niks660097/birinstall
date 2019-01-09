af98523baaf603b008b29ed0dfed388a011605c2
Nirmal Singh <niks660097@gmail.com>
2019-01-09 19:18:51 +0530

    Update exclusive_wallet_install.sh

--- exclusive_wallet_install.sh
+++ exclusive_wallet_install.sh
@@ -5,4 +5,6 @@ cd ExclusiveCoin/src
 sudo apt-get install libssl-dev
 sudo apt-get install libboost-dev
 sudo apt-get install libdb++-dev
+sudo apt-get remove qt3-dev-tools libqt3-mt-dev
+sudo apt-get install libqt4-dev libminiupnpc-dev
 sudo make -f makefile.unix
