5448e225436d97e45b0396e190773298fa9a5e91
Nirmal Singh <niks660097@gmail.com>
2019-01-14 16:01:13 +0530

    Create install_dependencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,14 @@
+sudo apt-get update
+sudo apt-get install automake
+sudo apt-get install libdb++-dev
+sudo apt-get install build-essential libtool autotools-dev
+sudo apt-get install autoconf pkg-config libssl-dev
+sudo apt-get install libboost-all-dev
+sudo apt-get install libminiupnpc-dev
+sudo apt-get install git
+sudo apt-get install software-properties-common
+sudo apt-get install python-software-properties
+sudo apt-get install g++
+sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo apt-get update
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
