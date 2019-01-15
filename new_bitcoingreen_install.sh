9c90a63e84d804c492ca9d5a5d1813333c11e0ff
Nirmal Singh <niks660097@gmail.com>
2019-01-15 19:47:40 +0530

    Delete new_bitcoingreen_install.sh

--- new_bitcoingreen_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget https://github.com/bitcoingreen/bitcoingreen/releases/download/v1.3.0/bitcoingreen-1.3.0-i686-pc-linux-gnu.tar.gz
-tar -xf bitcoingreen-1.3.0-i686-pc-linux-gnu.tar.gz
-cd ./bitcoingreen-1.3.0/bin
-mv bitcoingreen-cli /usr/local/bin
-mv bitcoingreend /usr/local/bin/
-mv bitcoingreen-tx /usr/local/bin
