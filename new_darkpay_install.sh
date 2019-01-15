94334f365510115fdabd2c83052bf2e988791988
Nirmal Singh <niks660097@gmail.com>
2019-01-15 17:16:56 +0530

    Create new_darkpay_install.sh

---
+++ new_darkpay_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://github.com/DarkPayCoin/darkpay/releases/download/v3.1.99/DarkPay-3.1.99-LINUX64.zip
+unzip DarkPay-3.1.99-LINUX64.zip
+mv darkpaycoin-cli /usr/local/bin
+mv darkpaycoind /usr/local/bin
