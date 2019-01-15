70400df2a73e9171502c0f5b8cb679fbb716a748
Nirmal Singh <niks660097@gmail.com>
2019-01-15 16:15:52 +0530

    Update vitae_install.sh

--- vitae_install.sh
+++ vitae_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get install wget
-wget https://github.com/VitaeTeam/Vitae/releases/download/4.3.0/vitae-4.3.0-linux-64.tar.gz"
+wget https://github.com/VitaeTeam/Vitae/releases/download/4.3.0/vitae-4.3.0-linux-64.tar.gz
 tar -xf vitae-4.3.0-linux-64.tar.gz
 cd vitae-4.3.0-linux-64
 mv vitae-cli /usr/local/bin/
