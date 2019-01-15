a6a4f0e074a1e6d58dc34e6c1d758446aff1c970
Nirmal Singh <niks660097@gmail.com>
2019-01-15 18:52:10 +0530

    Create new_goldpoker_install.sh

---
+++ new_goldpoker_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://github.com/GoldPoker/GoldPoker-coin/releases/download/v1.3.0/gpkr-1.3.0-x86_64-pc-linux-gnu.zip
+unzip gpkr-1.3.0-x86_64-pc-linux-gnu.zip
+mv gpkrd /usr/local/bin
+mv gpkr-cli /usr/local/bin
+mv gpkr-tx /usr/local/bin
