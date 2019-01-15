c8f4ba442dca337cc0a89a7536578060b2b75405
Nirmal Singh <niks660097@gmail.com>
2019-01-15 17:02:25 +0530

    Update new_exclusive_install.sh

--- new_exclusive_install.sh
+++ new_exclusive_install.sh
@@ -4,3 +4,4 @@ sudo apt-get install wget
 wget https://github.com/exclfork/ExclusiveCoin/releases/download/v1400/exclusivecoind-ubu14-v1400.zip
 unzip exclusivecoind-ubu14-v1400.zip
 mv exclusivecoind /usr/local/bin/
+chmod +x /usr/local/bin/exclusivecoind
