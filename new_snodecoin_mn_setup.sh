3eda2e7da31c1a068c5d819ad7d8504ea51cbb7e
Nirmal Singh <niks660097@gmail.com>
2019-01-15 17:28:48 +0530

    Create new_snodecoin_mn_setup.sh

---
+++ new_snodecoin_mn_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+FOLDER_PATH="/root/.snodecoin"
+CONF_FILE_PATH="/root/.snodecoin/snodecoin.conf"
+NODEIP=$(curl -s4 icanhazip.com)
+COIN_PORT=6667
+MASTERNODE_PRIVATE_KEY="$1"
+
+function update_config() {
+ RPCUSER=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w10 | head -n1)
+ RPCPASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w22 | head -n1)
+  cat << EOF > $CONF_FILE_PATH
+rpcuser=$RPCUSER
+rpcpassword=$RPCPASSWORD
+logintimestamps=1
+maxconnections=64
+masternode=1
+externalip=$NODEIP:$COIN_PORT
+masternodeprivkey=$MASTERNODE_PRIVATE_KEY
+EOF
+}
+mkdir $FOLDER_PATH
+touch $CONF_FILE_PATH
+update_config
+snodecoind -daemon
