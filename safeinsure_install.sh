06d804998dc743421796ede27ed1710747804ce4
Nirmal Singh <niks660097@gmail.com>
2019-01-15 16:27:23 +0530

    Create safeinsure_install.sh

---
+++ safeinsure_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://latest.safeinsure.io/safeinsure-x86_64-linux-gnu.tar.gz
+cd SafeInsure-1.0.0
+mv safeinsure-cli /usr/local/bin
+mv safeinsured /usr/local/bin
+mv safeinsure-tx /usr/local/bin
