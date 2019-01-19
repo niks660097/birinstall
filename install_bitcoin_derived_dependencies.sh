45bb2dd19e7e22583559006c66dc249586b78ccc
Nirmal Singh <niks660097@gmail.com>
2019-01-19 18:51:33 +0530

    Update install_bitcoin_derived_dependencies.sh

--- install_bitcoin_derived_dependencies.sh
+++ install_bitcoin_derived_dependencies.sh
@@ -1,4 +1,6 @@
 sudo apt-get update
+sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo apt-get update
 sudo apt-get install automake
 sudo apt-get install libdb++-dev
 sudo apt-get install build-essential libtool autotools-dev
@@ -9,6 +11,4 @@ sudo apt-get install git
 sudo apt-get install software-properties-common
 sudo apt-get install python-software-properties
 sudo apt-get install g++
-sudo add-apt-repository ppa:bitcoin/bitcoin
-sudo apt-get update
 sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
