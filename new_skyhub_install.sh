b7c31c40fd3a8347801aad955c09e88115d91209
Nirmal Singh <niks660097@gmail.com>
2019-02-06 17:05:26 +0530

    Update new_skyhub_install.sh

--- new_skyhub_install.sh
+++ new_skyhub_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://github.com/SkyHuBs/skyhub/releases/download/v1.1/SkyHub-1.1.0-x86_64-pc-linux-gnu.zip
-unzip gpkr-1.3.0-x86_64-pc-linux-gnu.zip
+unzip SkyHub-1.1.0-x86_64-pc-linux-gnu.zip
 mv skyhub-cli /usr/local/bin
 mv skyhubd /usr/local/bin
 my skyhub-tx /usr/local/bin
